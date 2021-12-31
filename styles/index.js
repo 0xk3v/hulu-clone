fc7d0ad6b2fc0204ca73670819efdde2b34a50a0
n-kev1n <kevin_@tuta.io>
2021-12-31 17:24:02 +0100

    Holder for all styles

--- styles/index.js
+++ styles/index.js
@@ -1,6 +1,19 @@
 import Hero, { HeroContainer } from "./hero.styled";
 import { Nav } from "./navbar.styled";
 import Button from "./button.styled";
-import Plans from "./plans.styled";
+import Plans, {
+  PlansContainer,
+  CardsContainer,
+  CardsItem,
+} from "./plans.styled";
 
-export { Hero, HeroContainer, Nav, Button, Plans };
+export {
+  Hero,
+  HeroContainer,
+  Nav,
+  Button,
+  Plans,
+  PlansContainer,
+  CardsContainer,
+  CardsItem,
+};
